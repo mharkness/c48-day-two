577b821394e986b8d6464bad615f1950915ae3a0
Morgan Harkness <Morgan Harkness>
2015-10-28 21:02:02 -0700

    first commit

---
+++ Javascriptexample.js
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+	<title>Fancy Javascript</title>
+</head>
+  <body>
+  <script src='javascript/example.js'></script>
+  </body>
---
+++ Javascriptexample/example.js
@@ -0,0 +1,13 @@
+alert("It works");
+console.log("this is the console log");
+var someNumber = 12;
+var anotherNumber = someNumber + 4;
+var quote = "A great man once said"
+console.log(someNumber);
+console.log(anotherNumber);
+console.log(quote);
+var favoriteFoods = ['pizza','ice cream','burgers'];
+console.log(favoriteFoods);
+console.log(favoriteFoods[0]);
+var thirdFav = favoriteFoods[2];
+console.log(thirdFav);
